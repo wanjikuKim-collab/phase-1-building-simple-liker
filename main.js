8e1d01677e71e1a64caacf484d89ba35a20cf463
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-09 21:24:30 +0300

    Completed

--- main.js
+++ main.js
@@ -3,6 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal');
+const likeGlyph = document.querySelectorAll('.like-glyph')
+
+modal.classList.add('hidden');
+
+for(item of likeGlyph){
+  item.addEventListener('click',(e)=>{
+    console.log(e.target)
+    let like= e.target;
+    heartEvent(like);
+    console.log(like);
+})
+}
+function heartEvent(like){
+  mimicServerCall().then(function(response){
+    heartClicked(like);
+    console.log("Success!");
+    console.log(response);
+  }).catch(error=>{
+    let modal = document.getElementById('modal')
+    modal.className = "";
+    console.log(error)
+
+    setTimeout(()=>{
+      modal.className="hidden"
+    },3000)
+  })
+
+}
+
+function heartClicked(like) {
+  if(like.innerHTML === EMPTY_HEART) {
+    //add class
+    like.classList.add('activated-heart');
+    like.innerHTML = FULL_HEART;
+    console.log("Success!");
+  }
+  else {
+    like.classList.remove('activated-heart');
+    like.innerHTML = EMPTY_HEART;
+  }
+}
 
 
 
